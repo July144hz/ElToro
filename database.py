0aba4557053b1e7225fad516af5ce219410386d3
TomasFernandez1 <fernandez.tomaset36@gmail.com>
2021-08-31 13:53:37 -0300

    no funciona el boton de mierda

--- database.py
+++ database.py
@@ -14,13 +14,18 @@ def tarjetas():
         database='python')
     fcursor = bd.cursor()
     def main():
-        global l1,l2,l3,e1,e2,btn1,fotoLogin,root
+        global l1,l2,l3,e1,e2,btn1,fotoLogin,root,btnsalir
         
         fondo='#71b4fd'
         root.config(bg=fondo)
         root.title("DataBase")
         root.geometry("500x500")
         root.resizable(False,False)
+        try:
+            btnsalir.destroy()
+        except:
+            btnsalir=Button()
+            btnsalir.destroy()
 
         logo = ImageTk.PhotoImage(Image.open('resources/bbv.png'))
         l1=Label(image=logo,bg=fondo)
@@ -60,7 +65,7 @@ def tarjetas():
             fcursor.execute("SELECT saldo FROM tarjetas WHERE usuario = '"+var1+"' and password = '"+var2+"'")
             for i in fcursor:
                 #print(i)
-                saldo = i[0]
+                saldo = int(i[0])
                 #print(saldo, type(saldo))
             fondo = "#7fffd4"
             
@@ -68,7 +73,7 @@ def tarjetas():
             l4=Label(pady=35,bg=fondo)
             l4.pack()
             def principal():
-                global btn2, btn1, btn3, btn4, l5, l4,btnsalir
+                global btn2, btn1, btn3, btn4, l5, l4,btnsalir1
                 l1.destroy(),l2.destroy(),l3.destroy(),btn1.destroy(),e1.destroy(),e2.destroy()
                 root.configure(bg=fondo)
                 l5=Label(bg=fondo,font=("bahnschrift",20),text="Que desea hacer?")
@@ -79,20 +84,24 @@ def tarjetas():
                 btn3.pack(pady=2)
                 btn4=Button(text="SALIR",image=salir,font=("bahnschrift",20),bg=fondo,activebackground=fondo,padx=75,borderwidth=0,highlightthickness=0,highlightcolor=fondo,command=lambda:(volverinicio()))
                 btn4.pack(pady=2)
-                btnsalir=Button(image=salir2,bg=fondo,activebackground=fondo,padx=75,borderwidth=0,highlightthickness=0,highlightcolor=fondo,command=lambda:(volverinicio()))
-                btnsalir.place(x=15,y=15)
+                btnsalir1=Button(image=salir2,bg=fondo,activebackground=fondo,padx=75,borderwidth=0,highlightthickness=0,highlightcolor=fondo,command=volverinicio)
+                btnsalir1.place(x=15,y=15)
             def volverinicio():
-                btn4.destroy(),btn3.destroy(),btn2.destroy(),l5.destroy(),l4.destroy(),btnsalir.destroy(),btnsalir.destroy()
+                global btnsalir1
+                btn4.destroy(),btn3.destroy(),btn2.destroy(),l5.destroy(),l4.destroy(),btnsalir1.destroy()
                 main()
 
             principal()
 
             def imonto():
+                global l8, btnsalir
                 l5.config(text="Que cantidad desea ingresar?")
                 monto= StringVar()
                 e3=Entry(textvariable=monto,justify=CENTER,font=('calibri',20))
                 e3.bind('<Return>', lambda x:ingreso(monto))
                 e3.pack()
+                btnsalir2=Button(image=salir2,bg=fondo,activebackground=fondo,padx=75,borderwidth=0,highlightthickness=0,highlightcolor=fondo,command=lambda:(btn5.destroy(),e3.destroy(),btn7.destroy(),l5.destroy(),l8.destroy(),btnsalir2.destroy(),principal()))
+                btnsalir2.place(x=15,y=15)
                 btn5=Button(text="INGRESAR",font=("bahnschrift",20),bg="#029D4D",activebackground="#029D4D",command=lambda:(l8.configure(text=f"Saldo actual:{saldo+20}"),ingreso(monto)))
                 btn5.pack()
                 btn7 = Button(text="SALIR",font=("bahnschrift",20),bg="#E33F2C",activebackground="#E33F2C",command=lambda:(btn5.destroy(),e3.destroy(),btn7.destroy(),l5.destroy(),l8.destroy(),principal()))
@@ -106,9 +115,10 @@ def tarjetas():
                 fcursor.execute("UPDATE tarjetas SET saldo = (saldo+'"+monto.get()+"') WHERE usuario = '"+var1+"' and password = '"+var2+"'")
                 try:
                     saldo= saldo + float(monto.get())
+                    l8.configure(text=f"Saldo actual:{saldo}")
                 except:
                     messagebox.showerror(message = "Ha ocurrido un error, intente ingresar otro valor.", title="BANCO BILBAO VIZCAYA")
-                pass
+                    l8.configure(text=f"Saldo actual:{saldo}")
 
             def rmonto():
                 l5.config(text="Que cantidad desea retirar?")
@@ -126,12 +136,13 @@ def tarjetas():
 
             def retiro(monto):
                 global saldo
-                fcursor.execute("UPDATE tarjetas SET saldo = (saldo - '"+monto.get()+"') WHERE usuario = '"+var1+"' and password = '"+var2+"'")
+                fcursor.execute("UPDATE tarjetas SET saldo = (saldo-'"+monto.get()+"') WHERE usuario = '"+var1+"' and password = '"+var2+"'")
                 try:
                     saldo = saldo - float(monto.get())
+                    l8.configure(text=f"Saldo actual:{saldo}")
                 except:
                     messagebox.showerror(message="Ha ocurrido un error, intente ingresar otro valor.", title="BANCO BILBAO VIZCAYA")
-                pass
+                
 
         else:
             messagebox.showerror(message="Error, Intente denuevo", title="BANCO BILBAO VIZCAYA")
